9a7b99d380eb1ebfde37c2586e3815c2048175d4
Eduardo Andrade <duh.eaa@gmail.com>
2025-07-28 23:42:21 -0300

    🔧 (svg): remove check.svg file and update index.ts to remove reference to it as it is no longer needed in the project

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,4 +1,3 @@
-export { default as CheckIcon } from "./check.svg";
 export { default as GoogleIcon } from "./google-icon.svg";
 export { default as Logo } from "./logo.svg";
 export { default as SearchIcon } from "./search.svg";
